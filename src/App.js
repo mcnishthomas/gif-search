68e306af7ae1232017e82de55842394546fbc3da
Thomas McNish <mcnishthomas@gmail.com>
2022-05-16 09:04:16 -0400

    fixed API bug

--- src/App.js
+++ src/App.js
@@ -4,35 +4,35 @@ import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
     this.state = {
       gifs: []
-    };
-  } 
+    }
+  }
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=F6dbdsD9RXRBIUX9HzVTt7sjbcUYTE3l')
       .then(response => response.json())
       .then(responseData => {
-        this.setState({ gifs: responseData.data });
+        this.setState({ gifs: responseData.data })
       })
       .catch(error => {
-        console.log('Error fetching and parsing data', error);
+        console.log('Error fetching and parsing data', error)
       });
   }
 
-  render() { 
+  render() {
     console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>
